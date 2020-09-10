6f9db52b287d1a76ec0e493ee89b1173ebf55be0
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2020-09-10 15:29:24 -0400

    Made second copy for alien project

---
+++ Experiments/Alien/js/script.js
@@ -0,0 +1,23 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(500, 500);
+  background(255, 0, 0);
+
+  point(20, 20);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
