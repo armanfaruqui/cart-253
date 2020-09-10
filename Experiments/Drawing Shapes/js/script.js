83b3bff97d9f1d8fb30e15bd36713a5d01765db1
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2020-09-10 15:00:17 -0400

    TESTING #0001

--- Experiments/Drawing Shapes/js/script.js
+++ Experiments/Drawing Shapes/js/script.js
@@ -9,7 +9,10 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500, 500);
+  background(255, 0, 0);
 
+  point(20, 20);
 }
 
 // draw()
