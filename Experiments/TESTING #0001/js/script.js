a5f80fee5aa3b71c33522890a46feb465e6f9e5f
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2020-09-10 16:33:53 -0400

    Uploaded the correct folder

---
+++ Experiments/TESTING #0001/js/script.js
@@ -0,0 +1,80 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(500, 500);
+
+  background(18, 23, 87);
+
+  // Foundation shapes
+  noStroke();
+  fill(176, 131, 7);
+  rect(0, 0, 250, 250);
+
+  noStroke();
+  fill(245, 245, 245);
+  rect(0, 250, 250, 250);
+  // Dark blue stripes
+  noStroke();
+  fill(11, 4, 59);
+  rect(300, 0, 10, 500);
+
+  noStroke();
+  fill(11, 4, 59);
+  rect(350, 0, 10, 500);
+
+  noStroke();
+  fill(11, 4, 59);
+  rect(400, 0, 10, 500);
+
+  noStroke();
+  fill(11, 4, 59);
+  rect(450, 0, 10, 500);
+  //mustard lines
+  stroke(130, 92, 4);
+  strokeWeight(4);
+  noFill();
+  rect(20, 20, 210, 210);
+
+  noFill();
+  stroke(130, 92, 4);
+  strokeWeight(4);
+  rect(50, 50, 100, 100);
+
+  stroke(130, 92, 4);
+  strokeWeight(4);
+  line(20, 20, 50, 50)
+
+  stroke(130, 92, 4);
+  strokeWeight(4);
+  line(150, 150, 230, 230);
+
+  stroke(130, 92, 4);
+  strokeWeight(4);
+  line(150, 50, 230, 20);
+
+  stroke(130, 92, 4);
+  strokeWeight(4);
+  line(50, 150, 20, 230);
+
+
+
+
+
+
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
